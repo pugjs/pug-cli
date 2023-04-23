ebc24ea201d0dbf5af59e40f0fd3471dc291e5ad
Saulo Vallory <saulo@toki.life>
2023-04-23 18:40:05 -0300

    Fix bugs and tests

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-var fs = require('fs');
-var path = require('path');
-var { program } = require('commander');
-var mkdirp = require('mkdirp');
-var chalk = require('chalk');
-var pug = require('pug');
-var yaml = require('js-yaml')
-var matter = require('gray-matter');
-var yaml = require('js-yaml')
+var fs = require("fs");
+var path = require("path");
+var { program } = require("commander");
+var mkdirp = require("mkdirp");
+var chalk = require("chalk");
+var pug = require("pug");
+var yaml = require("js-yaml");
+var matter = require("gray-matter");
 
 var basename = path.basename;
 var dirname = path.dirname;
@@ -23,112 +22,142 @@ var relative = path.relative;
 
 program
   .version(
-    'pug version: '     + require('pug/package.json').version + '\n' +
-    '@anduh/pug-cli version: ' + require(  './package.json').version
+    "pug version: " +
+      require("pug/package.json").version +
+      "\n" +
+      "pug-cli version: " +
+      require("./package.json").version
   )
-  .usage('[options] [dir|file ...]')
-  .option('-O, --obj <str|path>', 'JSON/JavaScript/YAML options object or file')
-  .option('-o, --out <dir>', 'output the rendered HTML or compiled JavaScript to <dir>')
-  .option('-p, --path <path>', 'filename used to resolve includes')
-  .option('-b, --basedir <path>', 'path used as root directory to resolve absolute includes')
-  .option('-P, --pretty', 'compile pretty HTML output')
-  .option('-c, --client', 'compile function for client-side')
-  .option('-n, --name <str>', 'the name of the compiled template (requires --client)')
-  .option('-D, --no-debug', 'compile without debugging (smaller functions)')
-  .option('-w, --watch', 'watch files for changes and automatically re-render')
-  .option('-E, --extension <ext>', 'specify the output file extension')
-  .option('-s, --silent', 'do not output logs')
-  .option('--name-after-file', 'name the template after the last section of the file path (requires --client and overriden by --name)')
-  .option('--doctype <str>', 'specify the doctype on the command line (useful if it is not specified by the template)')
+  .usage("[options] [dir|file ...]")
+  .option("-O, --obj <str|path>", "JSON/JavaScript/YAML options object or file")
+  .option(
+    "-o, --out <dir>",
+    "output the rendered HTML or compiled JavaScript to <dir>"
+  )
+  .option("-p, --path <path>", "filename used to resolve includes")
+  .option(
+    "-b, --basedir <path>",
+    "path used as root directory to resolve absolute includes"
+  )
+  .option("-P, --pretty", "compile pretty HTML output")
+  .option("-c, --client", "compile function for client-side")
+  .option(
+    "-n, --name <str>",
+    "the name of the compiled template (requires --client)"
+  )
+  .option("-D, --no-debug", "compile without debugging (smaller functions)")
+  .option("-w, --watch", "watch files for changes and automatically re-render")
+  .option("-E, --extension <ext>", "specify the output file extension")
+  .option("-s, --silent", "do not output logs")
+  .option(
+    "--name-after-file",
+    "name the template after the last section of the file path (requires --client and overriden by --name)"
+  )
+  .option(
+    "--doctype <str>",
+    "specify the doctype on the command line (useful if it is not specified by the template)"
+  );
 
-
-program.on('--help', function(){
-  console.log('  Examples:');
-  console.log('');
-  console.log('    # Render all files in the `templates` directory:');
-  console.log('    $ pug3 templates');
-  console.log('');
-  console.log('    # Create {foo,bar}.html:');
-  console.log('    $ pug3 {foo,bar}.pug');
-  console.log('');
-  console.log('    # Using `pug` over standard input and output streams');
-  console.log('    $ pug3 < my.pug > my.html');
-  console.log('    $ echo \'h1 Pug!\' | pug');
-  console.log('');
-  console.log('    # Render all files in `foo` and `bar` directories to `/tmp`:');
-  console.log('    $ pug3 foo bar --out /tmp');
-  console.log('');
-  console.log('    # Specify options through a string:');
+program.on("--help", function () {
+  console.log("  Examples:");
+  console.log("");
+  console.log("    # Render all files in the `templates` directory:");
+  console.log("    $ pug3 templates");
+  console.log("");
+  console.log("    # Create {foo,bar}.html:");
+  console.log("    $ pug3 {foo,bar}.pug");
+  console.log("");
+  console.log("    # Using `pug` over standard input and output streams");
+  console.log("    $ pug3 < my.pug > my.html");
+  console.log("    $ echo 'h1 Pug!' | pug");
+  console.log("");
+  console.log(
+    "    # Render all files in `foo` and `bar` directories to `/tmp`:"
+  );
+  console.log("    $ pug3 foo bar --out /tmp");
+  console.log("");
+  console.log("    # Specify options through a string:");
   console.log('    $ pug -O \'{"doctype": "html"}\' foo.pug');
-  console.log('    # or, using JavaScript instead of JSON');
-  console.log('    $ pug3 -O "{doctype: \'html\'}" foo.pug');
-  console.log('');
-  console.log('    # Specify options through a file:');
-  console.log('    $ echo "exports.doctype = \'html\';" > options.js');
-  console.log('    $ pug3 -O options.js foo.pug');
-  console.log('    # or, JSON works too');
+  console.log("    # or, using JavaScript instead of JSON");
+  console.log("    $ pug3 -O \"{doctype: 'html'}\" foo.pug");
+  console.log("");
+  console.log("    # Specify options through a file:");
+  console.log("    $ echo \"exports.doctype = 'html';\" > options.js");
+  console.log("    $ pug3 -O options.js foo.pug");
+  console.log("    # or, JSON works too");
   console.log('    $ echo \'{"doctype": "html"}\' > options.json');
-  console.log('    $ pug3 -O options.json foo.pug');
-  console.log('');
+  console.log("    $ pug3 -O options.json foo.pug");
+  console.log("");
 });
 
 program.parse(process.argv);
 
-var args = program.opts();
-var options = (args.obj) ? parseObj(args.obj) : {};
-
-if (program.obj) {
-  options = parseObj(program.obj);
-}
+var cmdOptions = program.opts();
+var pugOptions = cmdOptions.obj ? parseObj(cmdOptions.obj) : {};
 
 /**
  * Parse object either in `input` or in the file called `input`. The latter is
  * searched first.
  */
-function parseObj (input) {
+function parseObj(input) {
+  let err = `PARSING ${input}\n`;
   try {
-    resolved = require.resolve(input);
-    delete require[resolved]; // delete cache
+    const resolved = path.resolve(input);
     return require(resolved);
   } catch (e) {
-    var str;
+    err += `Require didn't work for ${input}: ${e}\n`;
+    let str;
     try {
-      str = fs.readFileSync(args.obj, 'utf8');
+      str = fs.readFileSync(cmdOptions.obj, "utf8");
     } catch (e) {
-      str = args.obj;
+      err += ` and ${cmdOptions.obj} doesn't exist\n`;
+      str = cmdOptions.obj;
     }
+
+    err += `Trying JSON.parse ${str}\n`;
     try {
       return JSON.parse(str);
     } catch (e) {
+      err += `JSON.parse ERROR ${e}\n`;
       try {
-        return yaml.load(str, 'utf-8');
-      } catch(e) {
-        return eval('(' + str + ')');
+        return yaml.load(str, "utf-8");
+      } catch (e) {
+        err += `YAML ERROR ${e}\n`;
+        err += `Trying EVAL ${str}`;
+
+        try {
+          return eval("(" + str + ")");
+        } catch (e) {
+          err += `EVAL ERROR: ${e}\n`;
+          console.error(err);
+          throw new Error(err);
+        }
       }
     }
   }
 }
 
 [
-  ['path', 'filename'],      // --path
-  ['debug', 'compileDebug'], // --no-debug
-  ['client', 'client'],      // --client
-  ['pretty', 'pretty'],      // --pretty
-  ['basedir', 'basedir'],    // --basedir
-  ['doctype', 'doctype'],    // --doctype
+  ["path", "filename"], // --path
+  ["debug", "compileDebug"], // --no-debug
+  ["client", "client"], // --client
+  ["pretty", "pretty"], // --pretty
+  ["basedir", "basedir"], // --basedir
+  ["doctype", "doctype"], // --doctype
 ].forEach(function (o) {
-  options[o[1]] = args[o[0]] !== undefined ? args[o[0]] : options[o[1]];
+  pugOptions[o[1]] =
+    cmdOptions[o[0]] !== undefined ? cmdOptions[o[0]] : pugOptions[o[1]];
 });
 
 // --name
 
-if (typeof args.name === 'string') {
-  options.name = args.name;
+if (typeof cmdOptions.name === "string") {
+  pugOptions.name = cmdOptions.name;
 }
 
 // --silent
 
-var consoleLog = args.silent ? function() {} : console.log;
+var consoleLog = cmdOptions.silent ? function () {} : console.log;
 
 // left-over args are file paths
 
@@ -140,40 +169,48 @@ var files = program.args;
 var watchList = {};
 
 // function for rendering
-var render = args.watch ? tryRender : renderFile;
+var render = cmdOptions.watch ? tryRender : renderFile;
 
 // compile files
 
 if (files.length) {
   consoleLog();
 
-  if (program.watch) {
-    if (program.obj && fs.existsSync(program.obj)) {
-      fs.watchFile(program.obj, {persistent: true, interval: 200}, function() {
-        consoleLog('  ' + chalk.yellow(program.obj) + ' ' + chalk.gray('changed'));
+  if (cmdOptions.watch) {
+    if (cmdOptions.obj && fs.existsSync(cmdOptions.obj)) {
+      fs.watchFile(
+        cmdOptions.obj,
+        { persistent: true, interval: 200 },
+        function () {
+          consoleLog(
+            "  " + chalk.yellow(cmdOptions.obj) + " " + chalk.gray("changed")
+          );
 
-        // update object without losing previous data
-        Object.assign(options, parseObj(options));
+          // update object without losing previous data
+          Object.assign(pugOptions, parseObj(pugOptions));
 
-        // then update all files
-        for (const [path, bases] of Object.entries(watchList)) {
-          if (watchList.hasOwnProperty(path)) {
-            bases.forEach(render);
+          // then update all files
+          for (const [path, bases] of Object.entries(watchList)) {
+            if (watchList.hasOwnProperty(path)) {
+              bases.forEach(render);
+            }
           }
         }
-      });
+      );
 
-      consoleLog('  ' + chalk.gray('watching') + ' ' + chalk.yellow(program.obj));
+      consoleLog(
+        "  " + chalk.gray("watching") + " " + chalk.yellow(cmdOptions.obj)
+      );
     }
 
-    process.on('SIGINT', function() {
+    process.on("SIGINT", function () {
       process.exit(1);
     });
   }
   files.forEach(function (file) {
     render(file);
   });
-// stdio
+  // stdio
 } else {
   stdin();
 }
@@ -186,12 +223,12 @@ if (files.length) {
 function watchFile(path, base, rootPath) {
   path = normalize(path);
 
-  var log = '  ' + chalk.gray('watching') + ' ' + chalk.cyan(path);
+  var log = "  " + chalk.gray("watching") + " " + chalk.cyan(path);
   if (!base) {
     base = path;
   } else {
     base = normalize(base);
-    log += '\n    ' + chalk.gray('as a dependency of') + ' ';
+    log += "\n    " + chalk.gray("as a dependency of") + " ";
     log += chalk.cyan(base);
   }
 
@@ -204,23 +241,26 @@ function watchFile(path, base, rootPath) {
 
   consoleLog(log);
   watchList[path] = [base];
-  fs.watchFile(path, {persistent: true, interval: 200},
-               function (curr, prev) {
-    // File doesn't exist anymore. Keep watching.
-    if (curr.mtime.getTime() === 0) return;
-    // istanbul ignore if
-    if (curr.mtime.getTime() === prev.mtime.getTime()) return;
-    watchList[path].forEach(function(file) {
-      tryRender(file, rootPath);
-    });
-  });
+  fs.watchFile(
+    path,
+    { persistent: true, interval: 200 },
+    function (curr, prev) {
+      // File doesn't exist anymore. Keep watching.
+      if (curr.mtime.getTime() === 0) return;
+      // istanbul ignore if
+      if (curr.mtime.getTime() === prev.mtime.getTime()) return;
+      watchList[path].forEach(function (file) {
+        tryRender(file, rootPath);
+      });
+    }
+  );
 }
 
 /**
  * Convert error to string
  */
 function errorToString(e) {
-  return e.stack || /* istanbul ignore next */ (e.message || e);
+  return e.stack || /* istanbul ignore next */ e.message || e;
 }
 
 /**
@@ -234,7 +274,7 @@ function tryRender(path, rootPath) {
     renderFile(path, rootPath);
   } catch (e) {
     // keep watching when error occured.
-    console.error(errorToString(e) + '\x07');
+    console.error(errorToString(e) + "\x07");
   }
 }
 
@@ -243,19 +283,23 @@ function tryRender(path, rootPath) {
  */
 
 function stdin() {
-  var buf = '';
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', function(chunk){ buf += chunk; });
-  process.stdin.on('end', function(){
-    var output;
-    if (options.client) {
-      output = pug.compileClient(buf, options);
-    } else {
-      var fn = pug.compile(buf, options);
-      var output = fn(options);
-    }
-    process.stdout.write(output);
-  }).resume();
+  var buf = "";
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", function (chunk) {
+    buf += chunk;
+  });
+  process.stdin
+    .on("end", function () {
+      var output;
+      if (pugOptions.client) {
+        output = pug.compileClient(buf, pugOptions);
+      } else {
+        var fn = pug.compile(buf, pugOptions);
+        var output = fn(pugOptions);
+      }
+      process.stdout.write(output);
+    })
+    .resume();
 }
 
 /**
@@ -274,25 +318,25 @@ function renderFile(path, rootPath) {
   // Found pug file
   if (stat.isFile() && isPug.test(path) && !isIgnored.test(path)) {
     // Try to watch the file if needed. watchFile takes care of duplicates.
-    if (args.watch) watchFile(path, null, rootPath);
-    if (args.nameAfterFile) {
-      options.name = getNameFromFileName(path);
+    if (cmdOptions.watch) watchFile(path, null, rootPath);
+    if (cmdOptions.nameAfterFile) {
+      pugOptions.name = getNameFromFileName(path);
     }
-    
+
     let page = matter.read(path);
-    options.filename = page.path;
-    if(page.data.layout){
-      page.extended = `extends ${options.includes}/${page.data.layout}
-${page.content}`;
+    pugOptions.filename = page.path;
+    if (page.data.layout) {
+      page.extended = `extends ${pugOptions.includes}/${page.data.layout}
+    ${page.content}`;
     } else {
       page.extended = page.content;
     }
-    options.page = page.data;
+    pugOptions.page = page.data;
 
-    var fn = options.client
-           ? pug.compileClient(page.extended, options)
-           : pug.compile(page.extended, options);
-    if (program.watch && fn.dependencies) {
+    var fn = pugOptions.client
+      ? pug.compileClient(page.extended, pugOptions)
+      : pug.compile(page.extended, pugOptions);
+    if (cmdOptions.watch && fn.dependencies) {
       // watch dependencies, and recompile the base
       fn.dependencies.forEach(function (dep) {
         watchFile(dep, path, rootPath);
@@ -301,14 +345,14 @@ ${page.content}`;
 
     // --extension
     var extname;
-    if (args.extension)   extname = '.' + args.extension;
-    else if (options.client) extname = '.js';
-    else if (args.extension === '') extname = '';
-    else                     extname = '.html';
+    if (cmdOptions.extension) extname = "." + cmdOptions.extension;
+    else if (pugOptions.client) extname = ".js";
+    else if (cmdOptions.extension === "") extname = "";
+    else extname = ".html";
 
     // path: foo.pug -> foo.<ext>
     path = path.replace(isPug, extname);
-    if (args.out) {
+    if (cmdOptions.out) {
       // prepend output directory
       if (rootPath) {
         // replace the rootPath of the resolved path with output directory
@@ -317,21 +361,26 @@ ${page.content}`;
         // if no rootPath handling is needed
         path = basename(path);
       }
-      path = resolve(args.out, path);
+      path = resolve(cmdOptions.out, path);
     }
     var dir = resolve(dirname(path));
     mkdirp.sync(dir);
-    var output = options.client ? fn : fn(options);
+    var output = pugOptions.client ? fn : fn(pugOptions);
     fs.writeFileSync(path, output);
-    consoleLog('  ' + chalk.gray('rendered') + ' ' + chalk.cyan('%s'), normalize(path));
-  // Found directory
+    consoleLog(
+      "  " + chalk.gray("rendered") + " " + chalk.cyan("%s"),
+      normalize(path)
+    );
+    // Found directory
   } else if (stat.isDirectory()) {
     var files = fs.readdirSync(path);
-    files.map(function(filename) {
-      return path + '/' + filename;
-    }).forEach(function (file) {
-      render(file, rootPath || path);
-    });
+    files
+      .map(function (filename) {
+        return path + "/" + filename;
+      })
+      .forEach(function (file) {
+        render(file, rootPath || path);
+      });
   }
 }
 
@@ -342,8 +391,10 @@ ${page.content}`;
  * @returns {String}
  */
 function getNameFromFileName(filename) {
-  var file = basename(filename).replace(/\.(?:pug|jade)$/, '');
-  return file.toLowerCase().replace(/[^a-z0-9]+([a-z])/g, function (_, character) {
-    return character.toUpperCase();
-  }) + 'Template';
+  var file = basename(filename).replace(/\.(?:pug|jade)$/, "");
+  return (
+    file.toLowerCase().replace(/[^a-z0-9]+([a-z])/g, function (_, character) {
+      return character.toUpperCase();
+    }) + "Template"
+  );
 }
